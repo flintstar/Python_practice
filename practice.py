da16af150762cb2d82dc2c10fa29da2df7bc44f8
Mukthar <Mukthar@mukthar>
2022-05-30 18:45:55 +0000

    added a new function
    Added a word counter function  to practice script

--- practice.py
+++ practice.py
@@ -1,4 +1,22 @@
 def p_test(name):
+    """ Prints out values"""
     return (f"Hello {name}, welcome")
 hello = p_test("Flint")
 print(hello)
+
+def word_count():
+    """ Takes a text document and returns a dictionary of words and their counts """
+    words = {}
+    file_name = input("Please input file name or path:  ")
+    with open(file_name,"r") as text_file:
+        text = text_file.read()
+        f_text = text.split()
+        #print(f_text)
+        for word in f_text:
+            if word not in words:
+                words[word]= 1
+            else:
+                words[word] = words[word] + 1
+    return words
+
+print (word_count())
