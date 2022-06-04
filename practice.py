c0c31c3457ccbaca1809dba3e5ceb40b3c80ea5a
Mukthar <Mukthar@mukthar>
2022-06-04 22:59:57 +0000

    attempted to add an error catching feature

--- practice.py
+++ practice.py
@@ -4,19 +4,25 @@ def p_test(name):
 hello = p_test("Flint")
 print(hello)
 
+
 def word_count():
     """ Takes a text document and returns a dictionary of words and their counts """
     words = {}
-    file_name = input("Please input file name or path:  ")
-    with open(file_name,"r") as text_file:
+    try:
+        file_name = input("Please input file name or path:  ")
+    except FileNotFoundError as err:
+        print (f"Error while reading file {err}, Please enter a valid file name or path")
+        return err
+    with open(file_name, "r") as text_file:
         text = text_file.read()
         f_text = text.split()
         #print(f_text)
         for word in f_text:
             if word not in words:
-                words[word]= 1
+                words[word] = 1
             else:
-                words[word] = words[word] + 1
+                words[word] += 1
+                
     return words
 
 print (word_count())
