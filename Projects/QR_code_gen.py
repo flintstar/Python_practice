71bfbeba07667ecdb2d4b0e183564996baa708a5
Mukthar <Mukthar@mukthar>
2024-09-22 23:13:53 +0000

    Basic QR code generator
    added comments to body of code

--- Projects/QR_code_gen.py
+++ Projects/QR_code_gen.py
@@ -1,10 +1,10 @@
-
+#importing required modules
 import pyqrcode
 import png
 
-
-url = input("Insert your link:   ")
+# Take input and convert it to a QR code
+url = input("Insert your link: ")
 qr_code = pyqrcode.create(url)
-qr_code.png("link", scale=6)
+qr_code.png("link.png", scale=6)
 # print(f"Please confirm the link entered:{url}")
 qr_code.show()
